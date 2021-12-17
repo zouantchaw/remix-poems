7adc8172141a69761952448ef23fd1431fce5722
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-17 16:07:40 -0500

    peoms index route

---
+++ app/routes/poems/index.jsx
@@ -0,0 +1,11 @@
+export default function PoemsIndexRoute() {
+  return (
+    <div>
+      <p>Here's a random peom:</p>
+      <p>
+        One last hope Forgiveness, you once promised me an eternity not but a
+        prayer a child, Put to rest, in my tomb
+      </p>
+    </div>
+  );
+}
