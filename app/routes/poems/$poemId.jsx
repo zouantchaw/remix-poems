0e658ea0c245d35ac7c6fef95a9cfded17c24cfa
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-17 16:18:34 -0500

    route to display poems by 'id'

---
+++ app/routes/poems/$poemId.jsx
@@ -0,0 +1,12 @@
+export default function PoemRoute() {
+  return (
+    <div>
+      <p>Here's your thoughtful poem:</p>
+      <p>
+        One last hope Heart why did it come to this? there is only loneliness
+        for when the time comes, darkness descends a child, there is only
+        animosity Dearly departed
+      </p>
+    </div>
+  );
+}
