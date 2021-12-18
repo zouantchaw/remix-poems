c298135feeff0380d5b4ff7eeff0f02f2e691ce3
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-18 10:53:45 -0500

    connected css

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,4 @@
-import { LiveReload, Outlet } from "remix";
+import { LiveReload, Outlet, Links } from "remix";
 
 export default function App() {
   return (
@@ -6,6 +6,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Lovely Poems</title>
+        <Links />
       </head>
       <body>
         <Outlet />
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,9 @@
+import stylesUrl from "../styles/index.css"
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+}
+
 export default function IndexRoute() {
   return <div>Hello index route</div>
 }
