9829151b3a7ac116e16da4ed9740c3866dda137d
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2021-12-18 15:09:36 -0500

    add styling modules

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,25 @@
 import stylesUrl from "../styles/index.css"
+import { Link } from "remix";
 
 export const links = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 }
 
 export default function IndexRoute() {
-  return <div>Hello index route</div>
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Poems!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="poems">Read Poems</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- app/routes/poems.jsx
+++ app/routes/poems.jsx
@@ -1,12 +1,51 @@
-import { Outlet } from "remix";
+import { Outlet, Link } from "remix";
+import stylesUrl from "../styles/poems.css"
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl
+    }
+  ];
+};
 
 export default function PoemsRoute() {
   return (
-    <div>
-      <h1>P😇ems</h1>
-      <main>
-        <Outlet />
+    <div className="poems-layout">
+      <header className="poems-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link
+              to="/"
+              title="Remix Poems"
+              aria-label="Remix Poems"
+            >
+              <span className="logo">😇</span>
+              <span className="logo-medium">P😇ems</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="poems-main">
+        <div className="container">
+          <div className="poems-list">
+            <Link to=".">Get a random poem</Link>
+            <p>Here are some poems to check out:</p>
+            <ul>
+              <li>
+                <Link to="some-joke-id">Hippo</Link>
+              </li>
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="poems-outlet">
+            <Outlet />
+          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
